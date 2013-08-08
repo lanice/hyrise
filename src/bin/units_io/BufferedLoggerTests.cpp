bed72bc4403e123647bda6648d3c2203eeab1fa8
Tim Berning <tim.berning@student.hpi.uni-potsdam.de>
2013-08-08 06:44:44 -0700

    adjusted logger tests to use multiple threads logging concurrently

--- src/bin/units_io/BufferedLoggerTests.cpp
+++ src/bin/units_io/BufferedLoggerTests.cpp
@@ -1,17 +1,29 @@
 #include "testing/test.h"
 #include "io/BufferedLogger.h"
 
+#include <thread>
+#include <vector>
+
 namespace hyrise {
 namespace io {
 
 class BufferedLoggerTests : public ::hyrise::Test {};
 
-TEST_F(BufferedLoggerTests, simple_log_test) {
+void bufferedLoggerTestWorker(uint64_t tx) {
   BufferedLogger::getInstance().logDictionary('a', 1, (int64_t)2, 3);
   BufferedLogger::getInstance().logDictionary('a', 1, 2.0f, 3);
   BufferedLogger::getInstance().logDictionary('a', 1, "zwei", 3);
-  BufferedLogger::getInstance().logValue(1,'a',2,3,4,{5,6,7});
-  BufferedLogger::getInstance().logCommit(1);
+  BufferedLogger::getInstance().logValue(tx,'a',2,3,4,{5,6,7});
+  BufferedLogger::getInstance().logCommit(tx);
+}
+
+TEST_F(BufferedLoggerTests, simple_log_test) {
+  std::vector<std::thread> threadpool;
+  uint64_t tx = 1;
+  for(int i=0; i<100; i++)
+    threadpool.push_back(std::thread(bufferedLoggerTestWorker, tx++));
+  for(auto it=threadpool.begin(); it!=threadpool.end(); ++it)
+    it->join();
 }
 
 }
--- src/bin/units_io/SimpleLoggerTests.cpp
+++ src/bin/units_io/SimpleLoggerTests.cpp
@@ -1,17 +1,29 @@
 #include "testing/test.h"
 #include "io/SimpleLogger.h"
 
+#include <thread>
+#include <vector>
+
 namespace hyrise {
 namespace io {
 
 class SimpleLoggerTests : public ::hyrise::Test {};
 
-TEST_F(SimpleLoggerTests, simple_log_test) {
+void simpleLoggerTestWorker(uint64_t tx) {
   SimpleLogger::getInstance().logDictionary('a', 1, (int64_t)2, 3);
   SimpleLogger::getInstance().logDictionary('a', 1, 2.0f, 3);
   SimpleLogger::getInstance().logDictionary('a', 1, "zwei", 3);
-  SimpleLogger::getInstance().logValue(1,'a',2,3,4,{5,6,7});
-  SimpleLogger::getInstance().logCommit(1);
+  SimpleLogger::getInstance().logValue(tx,'a',2,3,4,{5,6,7});
+  SimpleLogger::getInstance().logCommit(tx);
+}
+
+TEST_F(SimpleLoggerTests, simple_log_test) {
+  std::vector<std::thread> threadpool;
+  uint64_t tx = 1;
+  for(int i=0; i<100; i++)
+    threadpool.push_back(std::thread(simpleLoggerTestWorker, tx++));
+  for(auto it=threadpool.begin(); it!=threadpool.end(); ++it)
+    it->join();
 }
 
 }

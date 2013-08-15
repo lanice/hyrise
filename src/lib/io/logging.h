b852ffaf0c328e2214ce24281e5a90ced2bc030b
Tim Berning <tim.berning@student.hpi.uni-potsdam.de>
2013-08-15 05:24:42 -0700

    added NoLogger to allow for 'disabled' logging

--- src/lib/io/logging.h
+++ src/lib/io/logging.h
@@ -1,14 +1,18 @@
 #ifndef SRC_LIB_IO_LOGGING_H
 #define SRC_LIB_IO_LOGGING_H
 
-#include "io/SimpleLogger.h"
 #include "io/BufferedLogger.h"
+#include "io/NoLogger.h"
+#include "io/SimpleLogger.h"
 
 namespace hyrise {
 namespace io {
 
+  // Uncomment to disable logging
+  typedef NoLogger Logger;
+
   // Uncomment to use SimpleLogger
-  typedef SimpleLogger Logger;
+  //typedef SimpleLogger Logger;
 
   // Uncomment to use BufferedLogger
   //typedef BufferedLogger Logger;

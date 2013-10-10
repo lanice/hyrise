8a56d8a0b7b1254018da8141477da9e9e2d53cac
Leander Neiss <Leander.Neiss@student.hpi.uni-potsdam.de>
2013-10-10 13:58:41 +0200

    Updating docs

--- _static/hyrise.js
+++ _static/hyrise.js
@@ -1,10 +1,14 @@
 $(document).ready( function() {
     $('body').addClass('onload');
-     $('ul').each( function() {
-        if (!$(this).hasClass('nav-pills')) {
+    $('ul').each( function() {
+        if (!$(this).hasClass('topnav')) {
             $(this).addClass('list notTop');
         }
     });
     $('ul.notTop li').addClass('list-group-item');
     $('table').addClass('table');
+
+    $('#learnmore').click(function() {
+        $('html,body').stop().animate({scrollTop: $('#fullwidth').offset().top - $('#menu').height()}, 750);
+    });
 });

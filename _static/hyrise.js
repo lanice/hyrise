9cae0d5b50c7dd9396391a5e0fc70cf17c8ccbd9
Leander Neiss <leander.neiss@student.hpi.uni-potsdam.de>
2013-10-23 17:33:19 +0200

    Updating docs

--- _static/hyrise.js
+++ _static/hyrise.js
@@ -12,4 +12,13 @@ $(document).ready( function() {
         $('html,body').stop().animate({scrollTop: $('#fullwidth').offset().top - $('#menu').height()}, 750);
     });
     $('.main .notTop').not('.simple').hide();
+
+    $('#backToTop').affix({
+        offset: {
+          top: 700, 
+          bottom: 70
+        }
+    }).click( function() {
+        $('html,body').stop().animate({scrollTop: 0}, 750);
+    });
 });
